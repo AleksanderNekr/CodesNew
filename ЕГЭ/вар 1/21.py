17aa5c2ce6e63060671eb2cc9671c2a2fd064817
Alexander <aleksandr_nekrasov@outlook.com>
2021-06-15 14:52:42 +0500

    added 21 task

---
+++ ЕГЭ/вар 1/21.py
@@ -0,0 +1,16 @@
+def f(x, y, p):
+    if x + y >= 77 and (p == 3 or p == 5):
+        return True
+    elif x + y < 77 and p == 5:
+        return False
+    elif x + y >= 77:
+        return False
+    if p % 2 == 1:
+        return f(x + 1, y, p + 1) and f(x + y * 2, y, p + 1) and f(x, y + 1, p + 1) and f(x, y + x * 2, p + 1)
+    else:
+        return f(x + 1, y, p + 1) or f(x + y * 2, y, p + 1) or f(x, y + 1, p + 1) or f(x, y + x * 2, p + 1)
+
+
+for s in range(1, 68):
+    if f(9, s, 1):
+        print(s)
