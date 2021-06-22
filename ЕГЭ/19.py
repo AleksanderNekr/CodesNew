bb6f27b65e1b997dbcf18d745f7e5aa8bb1c4eda
Alexander <aleksandr_nekrasov@outlook.com>
2021-06-22 23:51:13 +0500

    fixed

--- ЕГЭ/19.py
+++ ЕГЭ/19.py
@@ -3,8 +3,12 @@ def f(x, p):
         return True
     elif x >= 60 and p != 3:
         return False
-    return f(x * 2, p + 1) \
-        or f(x + 3, p + 1)
+    if p % 2 == 0:
+        return f(x * 2, p + 1) \
+            or f(x + 3, p + 1)
+    else:
+        return f(x * 2, p + 1) \
+            and f(x + 3, p + 1)
 
 
 for s in range(2, 57):
