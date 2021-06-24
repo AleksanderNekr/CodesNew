748cbed32f517247478b82ce99269e0551f332b1
Alexander <aleksandr_nekrasov@outlook.com>
2021-06-24 18:12:45 +0500

    C#

--- ЕГЭ/19.py
+++ ЕГЭ/19.py
@@ -1,14 +1,16 @@
 def f(x, p):
-    if x >= 60 and p == 3:
+    if x >= 60 and p == 2:
         return True
-    elif x >= 60 and p != 3:
+    elif x >= 60 and p != 2:
         return False
-    if p % 2 == 0:
-        return f(x * 2, p + 1) \
-            or f(x + 3, p + 1)
-    else:
-        return f(x * 2, p + 1) \
-            and f(x + 3, p + 1)
+    # if p % 2 == 0:
+    #     return f(x * 2, p + 1) \
+    #         or f(x + 3, p + 1)
+    # else:
+    #     return f(x * 2, p + 1) \
+    #         and f(x + 3, p + 1)
+    return f(x * 2, p + 1) \
+        or f(x + 3, p + 1)
 
 
 for s in range(2, 57):
