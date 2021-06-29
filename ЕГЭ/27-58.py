2ca2176a0081007f505196b3031027344491beb6
Alexander <aleksandr_nekrasov@outlook.com>
2021-06-29 21:41:40 +0500

    Some simple edits

--- ЕГЭ/27-58.py
+++ ЕГЭ/27-58.py
@@ -1,4 +1,4 @@
-f = open('ЕГЭ/27-58a.txt')
+f = open('ЕГЭ/27-58b.txt')
 n = int(f.readline())
 
 k = [0, 0, 0]
@@ -7,7 +7,7 @@ for i in range(n):
     x = int(f.readline())
     k1 = k.copy()
     for i in range(3):
-        k1[(i+x) % 3] += k[i]
+        k1[(i + x) % 3] += k[i]
     k1[x % 3] += 1
     k = k1.copy()
 print(k[0])
